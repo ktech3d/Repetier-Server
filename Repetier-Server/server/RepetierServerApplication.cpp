84b71c3b36bb5cc63ffe24490fdd14bd3c313bee
repetier <darko371@googlemail.com>
2013-07-11 19:40:03 +0200

    Added Poco, Service/Daemon code changed to Poco, Mongoose replaced by Poco.

---
+++ Repetier-Server/server/RepetierServerApplication.cpp
@@ -0,0 +1,9 @@
+//
+//  RepetierServerApplication.cpp
+//  Repetier-Server
+//
+//  Created by Roland Littwin on 09.07.13.
+//
+//
+
+#include "RepetierServerApplication.h"
---
+++ Repetier-Server/server/RepetierServerApplication.h
@@ -0,0 +1,14 @@
+//
+//  RepetierServerApplication.h
+//  Repetier-Server
+//
+//  Created by Roland Littwin on 09.07.13.
+//
+//
+
+#ifndef __Repetier_Server__RepetierServerApplication__
+#define __Repetier_Server__RepetierServerApplication__
+
+#include <iostream>
+
+#endif /* defined(__Repetier_Server__RepetierServerApplication__) */
