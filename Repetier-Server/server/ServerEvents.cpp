bc8a509399bb2ee34af350692f3eb96415338a19
repetier <darko371@googlemail.com>
2013-07-27 09:54:10 +0200

    Log over websocket

---
+++ Repetier-Server/server/ServerEvents.cpp
@@ -0,0 +1,9 @@
+//
+//  ServerEvents.cpp
+//  Repetier-Server
+//
+//  Created by Roland Littwin on 23.07.13.
+//
+//
+
+#include "ServerEvents.h"
---
+++ Repetier-Server/server/ServerEvents.h
@@ -0,0 +1,14 @@
+//
+//  ServerEvents.h
+//  Repetier-Server
+//
+//  Created by Roland Littwin on 23.07.13.
+//
+//
+
+#ifndef __Repetier_Server__ServerEvents__
+#define __Repetier_Server__ServerEvents__
+
+#include <iostream>
+
+#endif /* defined(__Repetier_Server__ServerEvents__) */
